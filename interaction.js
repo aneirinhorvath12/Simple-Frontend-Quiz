b86556b64ebe0e73448cddc1be2cac1ee01bd8de
Aneirin Horvath <aneirinhorvath12@gmail.com>
2021-04-01 12:32:36 +0100

    Some Javascript functionality added

---
+++ interaction.js
@@ -0,0 +1,21 @@
+let score = 0;
+
+let giraffeButton = document.getElementById('Giraffe');
+let lionButton = document.getElementById('Lion');
+let whaleButton = document.getElementById('Whale');
+let qOneAnswer = document.getElementById('Question_1_Answer');
+
+giraffeButton.addEventListener('click', wrong);
+lionButton.addEventListener('click', wrong);
+whaleButton.addEventListener('click', correct);
+
+function wrong () {
+  qOneAnswer.innerHTML = 'pussy';
+  qOneAnswer.style.visibility = 'visible';
+}
+
+
+function correct () {
+  qOneAnswer.style.visibility = 'visible';
+  score += 1;
+}
